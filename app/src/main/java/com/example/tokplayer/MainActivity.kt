bf97afedc3a979927289a2835f66acd955dd18bd
zxyzwa-crypto <zxyzwa@gmail.com>
2026-05-10 13:28:58 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/tokplayer/MainActivity.kt
+++ app/src/main/java/com/example/tokplayer/MainActivity.kt
@@ -1,11 +1,79 @@
-package com.example.tiktokplayer
+```kotlin
+package com.example.tokplayer
+
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.pager.VerticalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            TikTokPlayer()
+        }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun TikTokPlayer() {
+
+    val context = LocalContext.current
+
+    val videos = remember {
+        getAllVideos(context)
+    }
+
+    val pagerState = rememberPagerState(
+        pageCount = { videos.size }
+    )
+
+    VerticalPager(
+        state = pagerState,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+    ) { page ->
+
+        VideoPage(
+            uri = videos[page],
+            play = pagerState.currentPage == page
+        )
+    }
+}
+
+@Composable
+fun VideoPage(uri: Uri, play: Boolean) {
+
     val context = LocalContext.current
 
     val exoPlayer = remember {
         ExoPlayer.Builder(context).build().apply {
 
             setMediaItem(MediaItem.fromUri(uri))
+
             prepare()
+
             repeatMode = ExoPlayer.REPEAT_MODE_ONE
         }
     }
@@ -68,3 +136,4 @@ fun getAllVideos(context: android.content.Context): List<Uri> {
 
     return videoList
 }
+```
